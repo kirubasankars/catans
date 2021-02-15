7695b2ae05c877baf1996623ed7e048bacea0be2
VETRI SELVI KIRUBA SANKAR <vetriselvi@localhost.localdomain>
2021-02-15 08:25:56 -0500

    bank testcases

--- bank_test.go
+++ bank_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestBankGive(t *testing.T) {
 	bank := NewBank()
@@ -9,4 +11,43 @@ func TestBankGive(t *testing.T) {
 		t.Log("expected to have 18, failed.")
 		t.Fail()
 	}
+}
+
+func TestBankTake(t *testing.T) {
+	bank := NewBank()
+	bank.cards[0] = 17
+	err := bank.Set(0, 1)
+	if err != nil || bank.cards[0] != 19 {
+		t.Log("expected to have 19, failed.")
+		t.Fail()
+	}
+}
+
+func TestBankBuyDevCard(t *testing.T) {
+	bank := NewBank()
+	bank.devCardIndex = 18
+	_, err := bank.BuyDevCard()
+	if err != nil || bank.devCardIndex != 16 {
+		t.Log("expected to have 17, failed.")
+		t.Fail()
+	}
+}
+
+func TestBank1(t *testing.T) {
+	bank := NewBank()
+	bank.cards[0] = 5
+
+	bank.Begin()
+	for i := 0; i < 6; i++ {
+		_, err := bank.Get(0, 1)
+		if err != nil {
+			bank.Rollback()
+			break
+		}
+	}
+
+	if bank.cards[0] != 5 {
+		t.Fail()
+	}
+
 }
