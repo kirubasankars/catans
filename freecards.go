7c3a253bd91a1e5a10766e3c32d0626e671668ec
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2021-02-28 09:01:31 -0500

    give away 3 free cards in start

--- freecards.go
+++ freecards.go
@@ -7,17 +7,20 @@ func (context *GameContext) giveInitialFreeCards() error {
 	for _, player := range context.Players {
 		r := rand.Intn(2)
 		indices := player.Settlements[r].Indices
-		cardType := context.Tiles[indices[0]][0]
-		context.Bank.Get(cardType, 1)
-		player.Cards[cardType]++
-		context.EventCardDistributed(player.ID, cardType, 1)
 
-		if len(indices) > 1 {
-			cardType := context.Tiles[indices[1]][0]
+		giveCard := func(idx int) {
+			cardType := context.Tiles[indices[idx]][0]
 			context.Bank.Get(cardType, 1)
 			player.Cards[cardType]++
-
 			context.EventCardDistributed(player.ID, cardType, 1)
+		}
+
+		giveCard(0)
+		if len(indices) > 1 {
+			giveCard(1)
+		}
+		if len(indices) >= 2 {
+			giveCard(2)
 		}
 	}
 	context.Bank.Commit()
